4be7c0a3283f3f87c9e29b83a78d2490bffc51d4
Kilian Koeltzsch <me@kilian.io>
2015-06-18 12:09:24 +0200

    Add sample city file

---
+++ cities/Sample_City.py
@@ -0,0 +1,13 @@
+from bs4 import BeautifulSoup
+
+# The URL for the page where the parking lots are listed
+data_url = "http://example.com"
+
+# Name of the city, just in case it contains umlauts which this filename shouldn't
+city_name = "Sample City"
+
+def parse_html(html):
+    soup = BeautifulSoup(html)
+
+    # Do everything necessary to scrape the contents of the html
+    # into a dictionary of the format specified by the schema.
