eb723da844b9502d71fbb12f9fb0c2df3545a6ac
Kilian Koeltzsch <me@kilian.io>
2015-06-19 13:30:02 +0200

    [Luebeck] scrape data
    god this is dirty...

--- cities/Luebeck.py
+++ cities/Luebeck.py
@@ -1,6 +1,5 @@
 __author__ = 'kilian'
 
-import requests
 from bs4 import BeautifulSoup
 import datetime
 
@@ -12,10 +11,40 @@ file_name = "Luebeck"
 def parse_html(html):
     soup = BeautifulSoup(html)
     data = {
-        "lots": []
+        "lots": {}
     }
 
-    # Letzte Aktualisierung auslesen
+    # get time last updated
     date_last_changed = datetime.datetime.strptime(soup.find("tr").find("strong").text, "Stand: %d.%m.%Y, %H:%M Uhr")
     data["last_changed"] = str(date_last_changed)
+
+    rows = soup.find_all("tr")
+    rows = rows[1:]  #
+    for row in rows:
+        if len(row.find_all("th")) > 0:
+            # This is a header row, save it for later
+            region_header = row.find("th", {"class": "head1"}).text
+            data["lots"][region_header] = []
+        else:
+            if row.find("td").text == "Gesamt":
+                continue
+            # This is a parking lot row
+            raw_lot_data = row.find_all("td")
+            if len(raw_lot_data) == 2:
+                data["lots"][region_header].append({
+                    "name": raw_lot_data[0].text
+                })
+            elif len(raw_lot_data) == 4:
+                data["lots"][region_header].append({
+                    "name": raw_lot_data[0].text,
+                    "count": int(raw_lot_data[1].text),
+                    "free": int(raw_lot_data[2].text)
+                })
     return data
+
+
+if __name__ == "__main__":
+    file = open("../tests/luebeck.html")
+    html_data = file.read()
+    file.close()
+    parse_html(html_data)
