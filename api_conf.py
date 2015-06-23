c4b5b3e5841dbb6290bfd5c8170ed45b1da128b6
Kilian Koeltzsch <me@kilian.io>
2015-06-23 14:51:17 +0200

    Fix bug about missing SERVER_CONF

--- api_conf.py
+++ api_conf.py
@@ -1,15 +1,14 @@
 from os.path import join, dirname
 import structs
 
-
 VERSION = '1.0'
 SOURCE_REPOSITORY = 'https://github.com/offenesdresden/ParkAPI'
 CACHE_DIRECTORY = join(dirname(__file__), 'cache')
 
 DEFAULT_SERVER = structs.ServerConf(
-    port = 5000,
-    host = 'localhost',
-    mail = ''
+    port=5000,
+    host='localhost',
+    mail=''
 )
 
-del join, dirname, tools
+del join, dirname
