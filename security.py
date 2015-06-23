c430aa52d22cb2aa97fa11b225047f5b52fa5326
Kilian Koeltzsch <me@kilian.io>
2015-06-23 19:30:53 +0200

    Fix bug '__init__' showing in list of cities

--- security.py
+++ security.py
@@ -1,2 +1,2 @@
 def file_is_allowed(file):
-    return file.endswith(".py") and "__init__" not in file.title() and "Sample_City" not in file.title()
+    return file.endswith(".py") and "__Init__" not in file.title() and "Sample_City" not in file.title()
