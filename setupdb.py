cc6b602a10221b2b69adcbd73a3b9a720f6d9f8e
Kilian Koeltzsch <me@kilian.io>
2015-06-29 19:33:23 +0200

    Add setupdb.py, it's time to work this!

---
+++ setupdb.py
@@ -0,0 +1,28 @@
+import psycopg2
+import configparser
+
+config = configparser.ConfigParser()
+config.read("config.ini")
+
+db_data = {
+    "host": config["Database"]["host"],
+    "name": config["Database"]["name"],
+    "user": config["Database"]["user"],
+    "pass": config["Database"]["pass"],
+    "port": config["Database"]["port"]
+}
+
+conn = psycopg2.connect(database=db_data["name"], user=db_data["user"], host=db_data["host"], port=db_data["port"],
+                        password=db_data["pass"])
+cursor = conn.cursor()
+
+cursor.execute('CREATE TABLE "public"."parkapi" ('
+               '"id" SERIAL,'
+               '"timestamp_updated" TIMESTAMP NOT NULL,'
+               '"timestamp_downloaded" TIMESTAMP NOT NULL,'
+               '"city" TEXT NOT NULL,"data" JSON NOT NULL,'
+               'PRIMARY KEY ("id"))'
+               'TABLESPACE "pg_default";')
+
+conn.commit()
+conn.close()
