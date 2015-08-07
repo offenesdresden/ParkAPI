df169f41872f7e422acd2abd1fa3b624e1262bb7
Jörg Thalheim <joerg@higgsboson.tk>
2015-08-07 16:14:37 +0200

    add index for sorting to parkapi table

--- park_api/setupdb.py
+++ park_api/setupdb.py
@@ -11,7 +11,8 @@ def main():
                        '"timestamp_downloaded" TIMESTAMP NOT NULL,'
                        '"city" TEXT NOT NULL,"data" JSON NOT NULL,'
                        'PRIMARY KEY ("id"))'
-                       'TABLESPACE "pg_default";')
+                       'TABLESPACE "pg_default";'
+                       'create index latest_scrape_index on parkapi (city, timestamp_downloaded DESC);')
         conn.commit()
 
 if __name__ == "__main__":
