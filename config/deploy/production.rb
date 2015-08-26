dee4ac304ca3b07e66414b445aaeae77554d2ed3
Jörg Thalheim <joerg@higgsboson.tk>
2015-08-27 00:10:27 +0200

    ensure right remote is set

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -8,7 +8,12 @@ server "parkendd.higgsboson.tk",
 namespace :release do
   desc "Merge master into stable, run tests"
   task :prepare do
+    remotes = %x[git remote].split(/\n/)
+    unless remotes.include?("offenesdresden")
+      sh "git", "remote", "add", "offenesdresden", "git@github.com/offenesdresden/ParkAPI.git"
+    end
     sh "git", "checkout", "stable"
+    sh "git", "pull", "offenesdresden", "stable"
     sh "git", "merge", "master"
     sh "python", "-m", "unittest", "discover", "tests"
     sh "python", "tests/validate-geojson.py", *Dir.glob("park_api/cities/*.geojson")
@@ -17,7 +22,7 @@ namespace :release do
   desc "Push release and switch back to master"
   task :push do
     sh "git", "checkout", "stable"
-    sh "git", "push", "origin"
+    sh "git", "push", "offenesdresden", "stable"
     sh "git" "checkout", "master"
   end
 end
