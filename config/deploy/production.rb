2278643392b6e6737163d0d81d9557ef8c44cb4c
Jörg Thalheim <joerg@higgsboson.tk>
2015-08-26 20:19:12 +0200

    capistrano work-flow documentation

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -4,3 +4,19 @@ server "parkendd.higgsboson.tk",
   user: "production",
   roles: %w{:app},
   port: 2222
+
+namespace :release do
+  desc "Merge master into stable, run tests"
+  task :prepare do
+    sh "git", "checkout", "stable"
+    sh "git", "merge", "master"
+    sh "python", "-m", "unittest", "discover", "tests"
+  end
+
+  desc "Push release and switch back to master"
+  task :push do
+    sh "git", "checkout", "stable"
+    sh "git", "push", "origin"
+    sh "git" "checkout", "master"
+  end
+end
