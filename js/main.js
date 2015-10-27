c9a5751f0daa73d9f4638d948579070e2f9400bf
Florian Schmidt <florian.schmidt.1994@icloud.com>
2015-10-27 19:02:10 +0100

    Add static javascript frontend using google maps
    * Add fullscreen map using gmaps.js
    * Display map in #map div in index.html
    * get all city urls from "/" of production server
    * get all lot data from all city urls
    * add data to map as marker if lot.coords is provided

---
+++ js/main.js
@@ -0,0 +1,39 @@
+(function() {
+'use strict';
+
+var map = new GMaps({
+  el: '#map',
+  lat: 51.05031,
+  lng: 13.73754
+});
+
+var baseurl = "https:///park-api.higgsboson.tk/";
+$.get(baseurl, function(data) {
+
+  var urls = _.map(data.cities, function (value, key) {
+    return baseurl+key;
+  });
+
+  _.forEach(urls, addDataToMap);
+});
+
+function addDataToMap(url) {
+  $.get(url, function (data) {
+    data.lots.forEach(function (lot) {
+      if(!lot.coords) {
+        console.log("no coordinates for lots found");
+        return;
+      }
+      map.addMarker({
+            lat: lot.coords.lat,
+            lng: lot.coords.lng,
+            title: lot.name,
+            infoWindow: {
+              content: "<p>"+lot.name+"<br/>Status: "+lot.state+"<br/>Freie Parkplätze: "+lot.free+"/"+lot.total
+            }
+      });
+    });
+  });
+}
+
+})();
