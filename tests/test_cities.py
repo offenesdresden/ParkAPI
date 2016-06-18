131f873888d6377fe1943e0f353ced6277f1a833
Kai Wieland <post@kai.gs>
2016-06-18 15:20:33 +0200

    Add Freiburg
    - Freiburg provides a geojson API, so no scraping here.
    - `last_updated` will always be the most recent date, as Freiburg
    specifies this for every lot individually.

--- tests/test_cities.py
+++ tests/test_cities.py
@@ -89,6 +89,10 @@ class CityTestCase(unittest.TestCase):
         city_name = "Hamburg"
         self.sanity_check(city_name, scrape_city(city_name, ".xml"))
 
+    def test_freiburg(self):
+        city_name = "Freiburg"
+        self.sanity_check(city_name, scrape_city(city_name, ".json"))
+
     def test_sample(self):
         city_name = "Sample_City"
         self.sanity_check(city_name, scrape_city(city_name))
