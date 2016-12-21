68ed9520589c211d54b7e0aa41344f1e33edcb05
Johannes Kliemann <jk@jkliemann.de>
2016-12-21 01:10:45 +0100

    Creating data object.

--- park_api/dump.py
+++ park_api/dump.py
@@ -17,15 +17,55 @@ def get_args():
 
 def create_query(city=None, year=None, month=None, week=None):
     base = "select city, data from parkapi where"
+    count = "select count(city) from parkapi where"
+    conditions = " "
     if week and month:
         raise ValueError("Month and Week cannot be specified together.")
     if city:
-        base += " city = '{0}' and".format(city)
+        conditions += "city = '{0}' and".format(city)
     if not year:
         year = gmtime()[0]
-    base += " extract(year from timestamp_downloaded) = '{0}'".format(year)
+    conditions += " extract(year from timestamp_downloaded) = '{0}'".format(year)
     if week:
-        base += " and extract(week from timestamp_downloaded) = '{0}'".format(week)
+        conditions += " and extract(week from timestamp_downloaded) = '{0}'".format(week)
     elif month:
-        base += " and extract(month from timestamp_downloaded) = '{0}'".format(month)
-    return base
+        conditions += " and extract(month from timestamp_downloaded) = '{0}'".format(month)
+    return (base + conditions, count + conditions)
+
+def get_data(cursor, query):
+    
+    cursor.execute(query[1])
+    count = cursor.fetchone()['count']
+    
+    cursor.execute(query[0])
+    table = []
+    for i in range(count):
+        table.append(cursor.fetchone())
+    
+    data = {}
+    
+    for row in table:
+        if not row['city'] in data.keys():
+            data[row['city']] = {}
+        
+        for lot in row['data']['lots']:
+            if not lot['id'] in data[row['city']].keys():
+                data[row['city']][lot['id']] = []
+            data[row['city']][lot['id']].append((row['data']['last_downloaded'], lot['free']))
+    
+    print(data)
+
+def main(args):
+    if args.month and args.week:
+        print("Month and Week cannot be specified together.")
+        exit(1)
+    
+    query = create_query(args.city, args.year, args.month, args.week)
+    
+    db.setup()
+    
+    with db.cursor() as cursor:
+        get_data(cursor, query)
+    
+    
+    
