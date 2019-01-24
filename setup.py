7dca15451e810f0949c1d8c7c976d0368869b95e
Johannes Kliemann <jk@jkliemann.de>
2019-01-24 12:11:27 +0100

    support pip >= 10 in setup

--- setup.py
+++ setup.py
@@ -7,7 +7,10 @@ from setuptools import setup, find_packages
 from codecs import open
 from os import path
 
-from pip.req import parse_requirements
+try: # for pip >= 10
+        from pip._internal.req import parse_requirements
+except ImportError: # for pip <= 9.0.3
+        from pip.req import parse_requirements
 
 here = path.abspath(path.dirname(__file__))
 
