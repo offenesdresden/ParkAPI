5a0db82c52615e710b2fa7a4df6d19fcb3ca00bc
August Quint <49277349+AugustQu@users.noreply.github.com>
2019-04-10 15:48:51 +0200

    Wiesbaden.py

--- park_api/cities/Sample_City.py
+++ park_api/cities/Sample_City.py
@@ -14,35 +14,43 @@ def parse_html(html):
     soup = BeautifulSoup(html, "html.parser")
 
     # last_updated is the date when the data on the page was last updated, it should be listed on most pages
-    last_updated = soup.select("p#last_updated")[0].text
+    stand=soup.select('span')    
+    # this gives you:
+    # in stand[0]: <span style="font-weight: normal; letter-spacing: 0px;">
+    #              Stand: 10.04.2019 15:09        </span>
+    # spliiting it gives you: u'10.04.2019', u'15:09'
+    # putting it together: u'10.04.2019  15:09'
+    last_updated_date=stand[0].text.strip().split()[1]
+    last_updated_time=stand[0].text.strip().split()[2]
+    last_updated = last_updated_date + "  " + last_updated_time
 
     data = {
         # convert_date is a utility function you can use to turn this date into the correct string format
-        "last_updated": convert_date(last_updated, "%d.%m.%Y %H:%M Uhr"),
+        "last_updated": convert_date(last_updated, "%d.%m.%Y %H:%M Uhr"),     # TODO
         # URL for the page where the scraper can gather the data
         "lots": []
     }
 
-    for tr in soup.find_all("tr"):
-        lot_name = tr.find("td", {"class": "lot_name"}).text
-        lot_free = tr.find("td", {"class": "lot_free"}).text
-        lot_total = tr.find("td", {"class": "lot_total"}).text
-
+    table=soup.select('table')
+    td = table[2].find_all("td")
+    i=0
+    while i < len(td):
         # please be careful about the state only being allowed to contain either open, closed or nodata
         # should the page list other states, please map these into the three listed possibilities
-        state = tr.find("td", {"class": "lot_state"}).text
+        state = tr.find("td", {"class": "lot_state"}).text    # TODO
 
-        lot = geodata.lot(lot_name)
+        lot = geodata.lot(td[i+1].text.strip())
         data["lots"].append({
-            "name": lot.name,
-            "free": lot_free,
-            "total": lot_total,
+            "name": td[i+1].text.strip(),
+            "free": int(td[i+2].text.split()[0]),
+            "total": int(td[i+2].text.split()[2]),
             "address": lot.address,
             "coords": lot.coords,
-            "state": state,
-            "lot_type": lot.type,
-            "id": lot.id,
+            "state": state,                  # TODO
+            "lot_type": lot.type,           
+            "id": lot.id,                    # TODO
             "forecast": False,
         })
+        i += 5
 
     return data
