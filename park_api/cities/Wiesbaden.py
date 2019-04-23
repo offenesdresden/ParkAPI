6d1e357fe17d90eba0da4ff9da4f5d07ffa1b4ea
Johannes Kliemann <jk@jkliemann.de>
2019-04-23 22:40:06 +0200

    fix comments and whitespace

--- park_api/cities/Wiesbaden.py
+++ park_api/cities/Wiesbaden.py
@@ -2,19 +2,12 @@ from bs4 import BeautifulSoup
 from park_api.util import convert_date
 from park_api.geodata import GeoData
 
-# This loads the geodata for this city if <city>.geojson exists in the same directory as this file.
-# No need to remove this if there's no geodata (yet), everything will still work.
 geodata = GeoData(__file__)
 
-# This function is called by the scraper and given the data of the page specified as source in geojson above.
-# It's supposed to return a dictionary containing everything the current spec expects. Tests will fail if it doesn't ;)
 def parse_html(html):
-
-    # BeautifulSoup is a great and easy way to parse the html and find the bits and pieces we're looking for.
     soup = BeautifulSoup(html, "html.parser")
 
-    # last_updated is the date when the data on the page was last updated, it should be listed on most pages
-    stand=soup.select('span')    
+    stand=soup.select('span')
     # this gives you:
     # in stand[0]: <span style="font-weight: normal; letter-spacing: 0px;">
     #              Stand: 10.04.2019 15:09        </span>
@@ -25,9 +18,7 @@ def parse_html(html):
     last_updated = last_updated_date + "  " + last_updated_time
 
     data = {
-        # convert_date is a utility function you can use to turn this date into the correct string format
         "last_updated": convert_date(last_updated, "%d.%m.%Y %H:%M"),
-        # URL for the page where the scraper can gather the data
         "lots": []
     }
 
@@ -41,7 +32,7 @@ def parse_html(html):
             parking_state = 'open'
             parking_free  = 0
             parking_total = 0
-            if ( 'geschlossen' in td[i+2].text ) : 
+            if ( 'geschlossen' in td[i+2].text ) :
                 parking_state = 'closed'
             else :
                 parking_free = int(td[i+2].text.split()[0])
