bd3069448bad0df2380ff85893b78d0ffe020310
Johannes Kliemann <jk@jkliemann.de>
2019-06-04 00:05:03 +0200

    use open data API for koeln

--- park_api/cities/Koeln.py
+++ park_api/cities/Koeln.py
@@ -1,4 +1,5 @@
-from bs4 import BeautifulSoup
+import json
+import datetime
 from park_api.util import convert_date
 from park_api.geodata import GeoData
 
@@ -9,70 +10,33 @@ geodata = GeoData(__file__)
 # This function is called by the scraper and given the data of the page specified as source in geojson above.
 # It's supposed to return a dictionary containing everything the current spec expects. Tests will fail if it doesn't ;)
 def parse_html(html):
-    # some helper-functions
-    def calcLotsFree( td ):
-        def calculate( value ):
-            if value == 'X': return 0
-            return int(value)
-        
-        images = td.find_all("img")
-        if ( images[3]['alt'] == '-') : return -1
-        free = calculate( images[0]['alt'])
-        free = free*10 + calculate( images[1]['alt'])
-        free = free*10 + calculate( images[2]['alt'])
-        free = free*10 + calculate( images[3]['alt'])
-        return free
-
-#   go on with main-function
-    # BeautifulSoup is a great and easy way to parse the html and find the bits and pieces we're looking for.
-    soup = BeautifulSoup(html, "html.parser")
-
-    # last_updated is the date when the data on the page was last updated, it should be listed on most pages
-    # suche: <span class="datetime">07.05.2019 10:50</span></p>
-    last_updated = soup.find( "span", class_="datetime").text
-
-    data = {
-        # convert_date is a utility function you can use to turn this date into the correct string format
-        "last_updated": convert_date(last_updated, "%d.%m.%Y %H:%M"),
-        # URL for the page where the scraper can gather the data
-        "lots": []
-    }
-
-    # everything is in table-objects
-    parking = soup.find_all( "table", class_="quarter")
-    for car_park in parking :
-        parking_quarter = car_park.find_all("tr")
-        for parking_building in parking_quarter[1:] :
-            car_parking  = parking_building.find_all("td")
-            parking_name = car_parking[1].text
-#            if   ('St. Martin (' in parking_name) : parking_name = 'Groß St. Martin (*)'
-#            elif ( 'Hohe Stra' in parking_name)   : parking_name = 'Hohe Straße'
-#            elif ( 'rzenich' in parking_name)     : parking_name = 'Gürzenich'
-#            elif ( 'ln Arcaden' in parking_name)  : parking_name = 'Köln Arcaden'
-#            elif ( 'ckenstra' in parking_name)    : parking_name = 'Brückenstraße'
-#            elif ( 'cilienstra' in parking_name)  : parking_name = 'Cäcilienstraße'
-#            elif ( 'Wolfsstra' in parking_name)   : parking_name = 'Wolfsstraße'
-#            elif ( 'DuMont Carr' in parking_name) : parking_name = 'DuMont Carré'
-#            elif ( 'ck/Mauspfad' in parking_name) : parking_name = 'Brück/Mauspfad'
-            # get the data
-            lot = geodata.lot(parking_name)
-            # calc available parking_lots
-            parking_state = 'open'
-            parking_free = calcLotsFree( car_parking[0])
-            if ( parking_free < 0 ) :
-                parking_free = 0
-                parking_state = 'nodata'
-
-            data["lots"].append({
-                "name":     parking_name,
-                "free":     parking_free,
-                "total":    lot.total,
-                "address":  lot.address,
-                "coords":   lot.coords,
-                "state":    parking_state,
-                "lot_type": lot.type,
-                "id":       lot.id,
-                 "forecast": False,
+    data = json.loads(html)
+    lots = {
+            "lots":[],
+            "last_updated":None
+            }
+    id_lots = {geodata.lots[n].aux: geodata.lots[n] for n in geodata.lots}
+    timestamps = []
+    for feature in data["features"]:
+        try:
+            lot = id_lots[feature["attributes"]["IDENTIFIER"]]
+            lots["lots"].append({
+                "coords":lot.coords,
+                "name":lot.name,
+                "total":lot.total,
+                "free":feature["attributes"]["KAPAZITAET"],
+                "state":"nodata" if feature["attributes"]["KAPAZITAET"] == -1 else "open",
+                "id":lot.id,
+                "lot_type":lot.type,
+                "address":lot.address,
+                "forecast":False,
+                "region":""
             })
+            timestamps.append(convert_date(feature["attributes"]["TIMESTAMP"], "%Y-%m-%d %H:%M:%S"))
+        except KeyError:
+            pass
+    timestamps.sort()
+    timestamps.reverse()
+    lots["last_updated"] = timestamps[0]
+    return lots
 
-    return data
