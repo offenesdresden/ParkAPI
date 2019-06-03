1948efbe164ace920d03e188f8aa1295ddaae8ed
Johannes Kliemann <jk@jkliemann.de>
2019-06-04 00:04:10 +0200

    tool to convert https://www.koeln.de/apps/parken/json/current into geojson

---
+++ tools/geojson_koeln.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python3
+
+import sys
+import json
+
+if __name__ == "__main__":
+    with open(sys.argv[1], "r") as f:
+        data = json.load(f)
+
+    geo = {"type":"FeatureCollection",
+           "features":[{
+               "type":"Feature",
+               "geometry":{
+                   "type":"Point",
+                   "coordinates":[
+                       6.958249,
+                       50.941387
+                   ]
+               },
+               "properties":{
+                   "name":"Köln",
+                   "type":"city",
+                   "url":"https://offenedaten-koeln.de/dataset/parkhausbelegung",
+                   "source":"https://www.stadt-koeln.de/externe-dienste/open-data/parking-ts.php",
+                   "active_support":"true"
+                }
+            }
+        ]
+    }
+
+    for k in data.keys():
+        geo["features"].append({
+            "type":"Feature",
+            "properties":{
+                "name":data[k]["title"].replace(" (*)", ""),
+                "total":data[k]["capacity"],
+                "address":data[k]["street"] + " " + data[k]["housenumber"],
+                "type":"Parkhaus",
+                "identifier":k
+            },
+            "geometry":{
+                "type":"Point",
+                "coordinates":[
+                    data[k]["lng"],
+                    data[k]["lat"]
+                ]
+            }
+        })
+
+    with open("Koeln.geojson", "w") as geojson:
+        json.dump(geo, geojson, indent=4, sort_keys=True, ensure_ascii=False)
+
