fad1b8b043214549ef46841e443fc6b7d4b24342
August Quint <49277349+AugustQu@users.noreply.github.com>
2019-07-06 17:38:09 +0200

    Update Rosenheim.py

--- park_api/cities/Rosenheim.py
+++ park_api/cities/Rosenheim.py
@@ -4,6 +4,7 @@ from park_api.geodata import GeoData
 from park_api.util import utc_now
 from park_api import env
 import urllib
+import json
 
 # This loads the geodata for this city if <city>.geojson exists in the same directory as this file.
 # No need to remove this if there's no geodata (yet), everything will still work.
