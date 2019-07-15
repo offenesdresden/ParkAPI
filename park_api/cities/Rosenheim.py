b8d9cecbd526323cdb41459f6415ac15be3d9551
Johannes Kliemann <jk@jkliemann.de>
2019-07-15 20:56:21 +0200

    use custom headers in rosenheim, remove invalid totals

--- park_api/cities/Rosenheim.py
+++ park_api/cities/Rosenheim.py
@@ -2,7 +2,6 @@ from bs4 import BeautifulSoup
 from park_api.geodata import GeoData
 from park_api.util import utc_now
 from park_api import env
-import urllib
 import json
 
 # This loads the geodata for this city if <city>.geojson exists in the same directory as this file.
@@ -13,23 +12,13 @@ geodata = GeoData(__file__)
 # It's supposed to return a dictionary containing everything the current spec expects. Tests will fail if it doesn't ;)
 def parse_html(html):
 
-    # BeautifulSoup is a great and easy way to parse the html and find the bits and pieces we're looking for.
-    soup = BeautifulSoup(html, "html.parser")
-
     data = {
         "last_updated": utc_now(),     # not found on site, so we use something else
         # URL for the page where the scraper can gather the data
         "lots": []
     }
 
-    # load the JSON-file:
-    urlHD = 'https://www.rosenheim.de/index.php?eID=jwParkingGetParkings'
-    headerHD={'Accept': 'application/json; charset=utf-8', 
-               'User-Agent': 'ParkAPI v%s - Info: %s' %(env.SERVER_VERSION, env.SOURCE_REPOSITORY) }
-    req = urllib.request.Request(url=urlHD, headers=headerHD)
-    webURL = urllib.request.urlopen(req)
-    dataRO=webURL.read()
-    dataJSON=json.loads(dataRO.decode('utf-8'))
+    dataJSON=json.loads(html)
     # over all parking-lots
     for parking_lot in dataJSON :
         parking_name = parking_lot['title']
@@ -47,7 +36,7 @@ def parse_html(html):
             data["lots"].append({
                     "name":     parking_name,
                     "free":     parking_free,
-                    "total":    lot.total,
+                    "total":    parking_lot['parkings'],
                     "address":  lot.address,
                     "coords":   lot.coords,
                     "state":    parking_status,
