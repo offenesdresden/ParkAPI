68999f2ed89b5cbd11d7dde149e29e74679eb05b
Kilian Koeltzsch <me@kilian.io>
2019-08-22 23:25:58 +0200

    Add FeedKit dependency

--- Package.swift
+++ Package.swift
@@ -10,12 +10,13 @@ let package = Package(
             targets: ["ParkImporter"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/scinfu/SwiftSoup", from: "2.2.0")
+        .package(url: "https://github.com/scinfu/SwiftSoup", from: "2.2.0"),
+        .package(url: "https://github.com/nmdias/FeedKit", from: "8.1.1"),
     ],
     targets: [
         .target(
             name: "ParkImporter",
-            dependencies: ["SwiftSoup"]),
+            dependencies: ["SwiftSoup", "FeedKit"]),
         .testTarget(
             name: "ParkImporterTests",
             dependencies: ["ParkImporter"]),
