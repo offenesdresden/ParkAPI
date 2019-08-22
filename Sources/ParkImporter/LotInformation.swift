7faed61681fce07df3d4b16af48edcaef4ea5710
Kilian Koeltzsch <me@kilian.io>
2019-08-22 23:34:25 +0200

    Add coordinates and address to Lot

--- Sources/ParkImporter/LotInformation.swift
+++ Sources/ParkImporter/LotInformation.swift
@@ -14,15 +14,21 @@ public struct LotInformation {
 
 public struct Lot {
     public let name: String
+    public let coordinates: Coordinates
+    public let address: String?
     public let free: Int
     public let state: State
     public let additionalInformation: [String: Any]?
 
     public init(name: String,
+                coordinates: Coordinates,
+                address: String?,
                 free: Int,
                 state: Lot.State,
                 additionalInformation: [String : Any]? = nil) {
         self.name = name
+        self.coordinates = coordinates
+        self.address = address
         self.free = free
         self.state = state
         self.additionalInformation = additionalInformation
@@ -34,5 +40,10 @@ extension Lot {
         case open
         case closed
         case noData
+    }
+
+    public struct Coordinates {
+        public let latitude: Double
+        public let longitude: Double
     }
 }
