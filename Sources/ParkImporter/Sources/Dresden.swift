281427b7f4b3b340c89681fd05a0d9d6dc5ac617
Kilian Koeltzsch <me@kilian.io>
2019-08-23 13:17:19 +0200

    [Dresden] select relevant tables

--- Sources/ParkImporter/Sources/Dresden.swift
+++ Sources/ParkImporter/Sources/Dresden.swift
@@ -7,8 +7,12 @@ public class Dresden: HtmlImporter {
     public let sourceURL = URL(string: "https://apps.dresden.de/ords/f?p=1110")!
 
     public func parse(html: String, response: URLResponse) throws -> DataPoint {
-        let doc: Document = try SwiftSoup.parse(html)
+        let doc = try SwiftSoup.parse(html)
         let dateSource = try doc.getElementById("P1_LAST_UPDATE")?.date(using: .dMy_Hms)
+
+        for table in try doc.select("table.uReportAlternative") {
+            print(try table.attr("summary"))
+        }
 
         return DataPoint(dateSource: dateSource, lots: [])
     }
