15d4380bc0caaeaa19f62f7fb9ad5145d459a8aa
Kilian Koeltzsch <me@kilian.io>
2019-08-23 13:41:07 +0200

    [Dresden] select correct data rows

--- Sources/ParkImporter/Sources/Dresden.swift
+++ Sources/ParkImporter/Sources/Dresden.swift
@@ -10,8 +10,16 @@ public class Dresden: HtmlImporter {
         let doc = try SwiftSoup.parse(html)
         let dateSource = try doc.getElementById("P1_LAST_UPDATE")?.date(using: .dMy_Hms)
 
-        for table in try doc.select("table.uReportAlternative") {
-            print(try table.attr("summary"))
+        // select all tables that have a summary set, e.g. a region identifier
+        for table in try doc.select("table[summary~=.+]") {
+            let region = try table.attr("summary")
+            if region == "Busparkplätze" { continue }
+
+            print("== \(region)")
+            for lotRow in try table.select("tr") {
+                print(try lotRow.text())
+            }
+            print()
         }
 
         return DataPoint(dateSource: dateSource, lots: [])
