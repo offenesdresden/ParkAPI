6fbb6bb4a3edbc0308ff554ed9df496a2b99135f
Kilian Koeltzsch <me@kilian.io>
2019-08-25 01:44:05 +0200

    Add CarPark.gatherData

--- Sources/CarPark/CarPark.swift
+++ Sources/CarPark/CarPark.swift
@@ -1,3 +1,5 @@
+import Foundation
+
 public enum CarPark {
     public static var importers: [BaseDataSource] = [
         Dresden()
@@ -9,5 +11,19 @@ public enum CarPark {
 
     public static var supportedSources: [String] {
         importers.map { $0.name }
+    }
+
+    public static func gatherData(session: URLSession = .shared,
+                                  completion: @escaping (Result<(BaseDataSource, DataPoint), ParkError>) -> Void) {
+        importers.forEach { importer in
+            importer.fetch(session: session) { result in
+                switch result {
+                case .failure(let error):
+                    completion(.failure(error))
+                case .success(let dataPoint):
+                    completion(.success((importer, dataPoint)))
+                }
+            }
+        }
     }
 }
