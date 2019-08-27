5e20e07971fb6c426e7a5dccf723300942cfd350
Kilian Koeltzsch <me@kilian.io>
2019-08-27 22:58:34 +0200

    Extend timespan request to also show historic data
    closes #93

---
+++ park_api/timespan.py
@@ -0,0 +1,46 @@
+import os
+import csv
+from datetime import datetime
+
+from park_api import db, env
+
+
+def timespan(city, lot_id, date_from, date_to):
+    now = datetime.now()
+    if date_from > now:
+        data = forecast(lot_id, date_from, date_to)
+    elif date_to < now:
+        data = known_timespan_data(city, lot_id, date_from, date_to)
+    else:
+        data = known_timespan_data(city, lot_id, date_from, now)
+        data.extend(forecast(lot_id, date_from, now))
+
+    return data
+
+
+def known_timespan_data(city, lot_id, date_from, date_to):
+    with db.cursor() as cur:
+        sql = '''SELECT timestamp_downloaded, data \
+        FROM parkapi \
+        WHERE timestamp_downloaded > %s AND timestamp_downloaded < %s AND city = %s'''
+        cur.execute(sql, (date_from, date_to, city,))
+        data = []
+        for row in cur.fetchall():
+            for lot in row['data']['lots']:
+                if lot['id'] == lot_id:
+                    data[row['timestamp_downloaded']] = lot['free']
+        return data
+
+
+def forecast(lot_id, date_from, date_to):
+    try:
+        csv_path = os.path.join(env.APP_ROOT, "forecast_data", lot_id + ".csv")
+
+        with open(csv_path) as csvfile:
+            data = []
+            for row in csv.reader(csvfile):
+                if date_from <= row[0] <= date_to:
+                    data[row[0]] = row[1]
+            return data
+    except FileNotFoundError:
+        return []
