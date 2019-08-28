9fecd83f53f9d62c3cbbd9d2698ce5f8e04bcaab
Johannes Kliemann <jk@jkliemann.de>
2019-08-28 23:43:22 +0200

    fix type errors

--- park_api/timespan.py
+++ park_api/timespan.py
@@ -6,7 +6,7 @@ from park_api import db, env
 
 
 def timespan(city, lot_id, date_from, date_to):
-    now = datetime.now()
+    now = datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%S")
     if date_from > now:
         data = forecast(lot_id, date_from, date_to)
     elif date_to < now:
@@ -14,7 +14,6 @@ def timespan(city, lot_id, date_from, date_to):
     else:
         data = known_timespan_data(city, lot_id, date_from, now)
         data.extend(forecast(lot_id, date_from, now))
-
     return data
 
 
@@ -24,11 +23,11 @@ def known_timespan_data(city, lot_id, date_from, date_to):
         FROM parkapi \
         WHERE timestamp_downloaded > %s AND timestamp_downloaded < %s AND city = %s'''
         cur.execute(sql, (date_from, date_to, city,))
-        data = []
+        data = {}
         for row in cur.fetchall():
             for lot in row['data']['lots']:
                 if lot['id'] == lot_id:
-                    data[row['timestamp_downloaded']] = lot['free']
+                    data[row['timestamp_downloaded'].strftime("%Y-%m-%dT%H:%M:%S")] = lot['free']
         return data
 
 
@@ -37,7 +36,7 @@ def forecast(lot_id, date_from, date_to):
         csv_path = os.path.join(env.APP_ROOT, "forecast_data", lot_id + ".csv")
 
         with open(csv_path) as csvfile:
-            data = []
+            data = {}
             for row in csv.reader(csvfile):
                 if date_from <= row[0] <= date_to:
                     data[row[0]] = row[1]
