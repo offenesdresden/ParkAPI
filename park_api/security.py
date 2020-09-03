6ab3857bdaa792a3e9e07cf63787942325d219a0
Johannes Kliemann <jk@jkliemann.de>
2020-09-03 18:12:15 +0200

    Disable Aalborg
    ref #212

--- park_api/security.py
+++ park_api/security.py
@@ -3,4 +3,5 @@ def file_is_allowed(file):
     t &= "__Init__" not in file.title()
     t &= "Sample_City" not in file.title()
     t &= "Frankfurt" not in file.title() # See offenesdresden/ParkAPI#153
+    t &= "Aalborg" not in file.title() # See offenesdresden/ParkAPI#212
     return t
