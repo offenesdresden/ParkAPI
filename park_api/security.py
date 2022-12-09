f7ccadd33b047bd00e7b8c2843007a50180541fa
Johannes Kliemann <jk@jkliemann.de>
2022-12-09 12:22:35 +0100

    Enable Wiesbaden
    ref #241

--- park_api/security.py
+++ park_api/security.py
@@ -4,5 +4,4 @@ def file_is_allowed(file):
     t &= "Sample_City" not in file.title()
     t &= "Frankfurt" not in file.title() # See offenesdresden/ParkAPI#153
     t &= "Aalborg" not in file.title() # See offenesdresden/ParkAPI#212
-    t &= "Wiesbaden" not in file.title() # See offenesdresden/ParkAPI#176
     return t
