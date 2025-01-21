464b186ffa7cfb8484d0131a448e10f5c7105c34
Renato Farruggio <renato.farruggio@bluewin.ch>
2025-01-21 15:46:50 +0100

    Update source of Basel data and adapt transforming code accordingly

--- park_api/cities/Basel.py
+++ park_api/cities/Basel.py
@@ -1,75 +1,47 @@
-import feedparser
-import html
+import json
 from datetime import datetime
+from datetime import timezone
 from park_api.geodata import GeoData
-from park_api.util import utc_now
 
 geodata = GeoData(__file__)
 
 
 def parse_html(xml_data):
-    feed = feedparser.parse(xml_data)
+    feed = json.loads(xml_data)
 
-    try:
-        last_updated = feed["entries"][0]["updated"]
-        last_updated = datetime.strptime(last_updated[5:25], "%d %b %Y %H:%M:%S").isoformat()
-    except KeyError:
-        last_updated = utc_now()
-
-
+    last_updated = feed['results'][0]['published']
+    last_updated = last_updated[:-3] + last_updated[-2:]
+    last_updated = datetime.strptime(last_updated, '%Y-%m-%dT%H:%M:%S%z').astimezone(timezone.utc).isoformat()[:-6]
 
     data = {
         "lots": [],
         "last_updated": last_updated
     }
 
-    for entry in feed["entries"]:
-        summary = parse_summary(entry["summary"])
-        title_elements = parse_title(entry["title"])
-
-        lot_identifier = html.unescape((title_elements[2] + " " + title_elements[0]).strip())
-        lot = geodata.lot(lot_identifier)
+    for entry in feed['results']:
 
         data["lots"].append({
-            "name": html.unescape(title_elements[0]),
-            "address": lot.address,
-            "id": html.unescape(lot.id),
-            "state": "open",
-            "free": summary[1],
-            "total": lot.total,
-            "coords": lot.coords,
+            "name": entry['name'],
+            "address": entry['address'],
+            "id": entry['id'],
+            "state": translate_status(entry['status']),
+            "free": entry['free'],
+            "total": entry['total'],
+            "coords": list(entry['geo_point_2d'].values()),
             "forecast": False,
-            "lot_type": title_elements[2]
+            "lot_type": entry['lot_type']
         })
+
     return data
 
+def translate_status(status: str) -> str:
+    state = None
 
-def parse_summary(summary):
-    """Parse a string from the format 'Anzahl freie Parkpl&auml;tze: 179' into both its params"""
-    summary = summary.split(":")
+    if status == 'offen':
+        state = 'open'
+    elif status == 'zu':
+        state = 'closed'
+    else:
+        state = 'unknown state'
 
-    summary[0] = summary[0].strip()
-    if "?" in summary[0]:
-        summary[0] = "nodata"
-
-    try:
-        summary[1] = int(summary[1])
-    except ValueError:
-        summary[1] = 0
-    return summary
-
-
-def parse_title(title):
-    """
-    Parse a string from the format 'Parkhaus Bad. Bahnhof'
-    """
-    types = ["Parkhaus", "Parkplatz"]
-
-    name = title
-    address = ''
-    type = ""
-    if name.split(" ")[0] in types:
-        type = name.split(" ")[0]
-        name = " ".join(name.split(" ")[1:])
-
-    return name, address, type
+    return state
